81efbbf462057d14a2d6b777845f435b0cc6a865
AZ <47402666+realazaber@users.noreply.github.com>
2024-04-09 12:52:55 +0200

    Chore: Tailwind setup

--- fetchgit/nuxt.config.ts
+++ fetchgit/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/styles/main.scss"],
   devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
---
+++ fetchgit/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3c82f6",
+      },
+    },
+  },
+  plugins: [],
+};
